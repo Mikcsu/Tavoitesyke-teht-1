9e5477ad7c76670a828e1878625da928cf352680
Mikcsu <miika.puominen@gmail.com>
2024-01-08 14:31:34 +0200

    viikkotehtävä 1

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState('');
+
+  let lower = (220 - number) * 0.65;
+  let upper = (220 - number) * 0.85;
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        onChangeText={(value) => setNumber(value)}
+        value={number}
+      />
+      <Text>Hr limits</Text>
+      <Text>{lower.toFixed()} - {upper.toFixed()}</Text>
       <StatusBar style="auto" />
     </View>
   );
